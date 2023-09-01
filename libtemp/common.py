d3a3da904eec0eef59e11bcbdb9d225dbe79893e
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2023-09-01 16:00:40 +0200

    Update CI workflow job name. Leverage fstring in common print function.

--- libtemp/common.py
+++ libtemp/common.py
@@ -4,7 +4,7 @@ from typing import List
 
 def print_hello_to(subject: str) -> None:
     """ Prints hello to a subject. """
-    print("hello {0}".format(subject))
+    print(f"hello {subject}")
 
 def concatenate_strings(strings: List[str]) -> str:
     """ Concatenates a list of strings into a string. """
